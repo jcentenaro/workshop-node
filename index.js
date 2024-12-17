4d12420c6d5685ae7c00731d34de224857fa88f0
Juan Centenaro <87022082+jcentenaro@users.noreply.github.com>
2024-12-17 12:11:04 -0300

    ejs + layouts

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const app = express();
+const expressLayouts = require("express-ejs-layouts");
 // CREO CARPETA PUBLIC Y ACÁ DIGO EN DONDE SE ENCUENTRA
 app.use(express.static("public"));
-// NPM I EJS
+// LE PIDO QUE USE LAS CONST QUE AGREGO ARRIBA X EJEMPLO AL INSALAR NPM I EJS
 app.set("view engine", "ejs");
 app.set("views", "./src/views");
+// LE DIGO QUE UTILICE LAYOUTS Y EN DONDE
+app.use(expressLayouts);
+app.set("layout", "layouts/layout")
 // DEFINO EL ROUTE QUE MANEJARÁ ESTE INDEX
 app.use(require("./src/routes/mainRoutes"));
 
-const PORT = 3000
+const PORT = 3001
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
